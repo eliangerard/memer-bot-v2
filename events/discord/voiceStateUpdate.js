4ef8caf32d99a1313994994bb82d5c3218937b41
Elian <eliangerardiso@gmail.com>
2022-08-04 11:49:37 -0600

    'Adapatation have been forgotted'

--- events/discord/voiceStateUpdate.js
+++ events/discord/voiceStateUpdate.js
@@ -6,7 +6,7 @@ module.exports = {
 	once: false,
 	execute(oldState, newState, client) {
 		if(newState.member == newState.guild.members.me && newState.channel != null)
-            console.log(getVoiceConnection(interaction.guildId) + " - " + getGroups().forEach(value => console.log(value)));
+            console.log(getVoiceConnection(newState.guild.id) + " - " + getGroups().forEach(value => console.log(value)));
 		    addSpeechEvent(client, { group: '1004267879933415444',lang: client.config.lang });
 	},
 };
