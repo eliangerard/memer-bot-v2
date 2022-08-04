2aea1927b49d584366fdc81efd64cb26e380328c
Elian <eliangerardiso@gmail.com>
2022-08-04 11:30:59 -0600

    Cleaned logs and preparing text based interaction

--- events/distube/addSong.js
+++ events/distube/addSong.js
@@ -3,19 +3,20 @@ const { EmbedBuilder } = require("discord.js");
 module.exports = {
 	name: 'addSong',
 	execute(queue, song, client) {
-        console.log('cancion añadida');
         if(queue.songs.length == 0) return;
-        console.log('si pasó');
+        
         const embed = new EmbedBuilder()
             .setTitle(client.emotes.success + " Añadiendo")
             .setColor("#FFFFFF")
             .setDescription(`${song.name} - \`${song.formattedDuration}\` | añadida por: ${song.user}`)
             .setTimestamp()
             .setFooter({text:'Memer', iconURL: client.botURL});
+
         if(!client.interaction)
             return client.channel.send({ embeds: [embed] }).then(msg => {
                 setTimeout(() => msg.delete(), 15000)
             });
+
         return client.interaction.editReply({ embeds: [embed] }).then(msg => {
             setTimeout(() => msg.delete(), 15000)
         });
