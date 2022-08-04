844c87c7e68bd3448063c0480dfabbd9efbc40f0
Elian <eliangerardiso@gmail.com>
2022-08-04 12:36:29 -0600

    Added undeaf by default and proper relative group distinction for speech event

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,7 +1,10 @@
+const { addSpeechEvent } = require("discord-speech-recognition");
+
 module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		addSpeechEvent(client, { group: client.user.id , lang: client.config.lang });
 	},
 };
--- events/discord/voiceStateUpdate.js
+++ events/discord/voiceStateUpdate.js
@@ -1,12 +1,11 @@
-const { getVoiceConnection, getGroups, joinVoiceChannel } = require('@discordjs/voice');
-const { addSpeechEvent } = require("discord-speech-recognition");
-
 module.exports = {
 	name: 'voiceStateUpdate',
 	once: false,
 	execute(oldState, newState, client) {
-		if(newState.member == newState.guild.members.me && newState.channel != null)
-            console.log(getVoiceConnection(newState.guild.id) + " - " + getGroups().get(1));
-		    addSpeechEvent(client, { group: '1004267879933415444',lang: client.config.lang });
+        console.log('hola');
+        const voiceConnection = client.distube.voices.get(newState.channel);
+		if(newState.member == newState.guild.members.me && newState.channel != null && voiceConnection.selfDeaf){
+            voiceConnection.setSelfDeaf(false);
+        }
 	},
 };
