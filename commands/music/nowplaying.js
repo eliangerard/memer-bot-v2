1629c7fcd3ef941c6990c45597719a18045208ef
Elian <eliangerardiso@gmail.com>
2022-08-04 14:23:54 -0600

    Fixed nowplaying

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -5,19 +5,32 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('np')
         .setDescription('Muestra lo que se está reproduciendo en este momento'),
-    inVoice : true,
+    inVoice : false,
     voiceCommand : ['cuál','cual'],
     voiceParams : false,
 	async execute(interaction, client) {
-        const queue = client.distube.getQueue(message)
+        interaction.deferReply();
+        const queue = client.distube.getQueue(interaction.guild);
+        if (!queue) {
+            const embed = new EmbedBuilder()
+            .setTitle(client.emotes.error+" Error")
+            .setColor("#FF0000")
+            .setDescription("No se está reproduciendo nada")
+            .setTimestamp()
+            .setFooter({text: 'Memer', iconURL: client.botURL});
+
+            return interaction.editReply( { embeds: [embed] } ).then(msg => {
+                setTimeout(() => msg.delete(), 15000)
+            });
+        }
         try {
             const embed = new EmbedBuilder()
                 .setTitle(client.emotes.play + " Reproduciendo")
                 .setColor("#FFFFFF")
                 .addFields(
                     { name: "Canción: ", value: queue.songs[0].name },
-                    { name: "Duración: ", value: queue.songs[0].formattedDuration, inline: true },
-                    { name: "Tiempo: ", value: queue.songs[0].formattedCurrentTime, inline: true},
+                    { name: "Duración: ", value: queue.formattedDuration, inline: true },
+                    { name: "Tiempo: ", value: queue.formattedCurrentTime, inline: true},
                     { name: "Solicitada por: ", value: "<@!" + queue.songs[0].user + ">", inline: true },
                     { name: "Ajustes: ", value: client.distube.status(queue) }
                 )
@@ -25,7 +38,7 @@ module.exports = {
                 .setTimestamp()
                 .setFooter({text: 'Memer', iconURL: client.botURL});
 
-            interaction.reply({ embeds: [embed] }).then(msg => {
+            interaction.editReply({ embeds: [embed] }).then(msg => {
                 setTimeout(() => msg.delete(), 15000)
             });
         }
@@ -37,21 +50,33 @@ module.exports = {
                 .setTimestamp()
                 .setFooter({text: 'Memer', iconURL: client.botURL});
 
-            interaction.reply({ embeds: [embed] }).then(msg => {
+            interaction.editReply({ embeds: [embed] }).then(msg => {
                 setTimeout(() => msg.delete(), 15000)
             });
         }
 	},
     async executeVoice (content, msg, client) {
-        const queue = client.distube.getQueue(msg.guild)
+        const queue = client.distube.getQueue(msg.guild);
+        if (!queue) {
+            const embed = new EmbedBuilder()
+            .setTitle(client.emotes.error+" Error")
+            .setColor("#FF0000")
+            .setDescription("No se está reproduciendo nada")
+            .setTimestamp()
+            .setFooter({text: 'Memer', iconURL: client.botURL});
+
+            return client.channel.send( { embeds: [embed] } ).then(msg => {
+                setTimeout(() => msg.delete(), 15000)
+            });
+        }
         try {
             const embed = new EmbedBuilder()
                 .setTitle(client.emotes.play + " Reproduciendo")
                 .setColor("#FFFFFF")
                 .addFields(
                     { name: "Canción: ", value: queue.songs[0].name },
-                    { name: "Duración: ", value: queue.songs[0].formattedDuration, inline: true },
-                    { name: "Tiempo: ", value: queue.songs[0].formattedCurrentTime, inline: true},
+                    { name: "Duración: ", value: queue.formattedDuration, inline: true },
+                    { name: "Tiempo: ", value: queue.formattedCurrentTime, inline: true},
                     { name: "Solicitada por: ", value: "<@!" + queue.songs[0].user + ">", inline: true },
                     { name: "Ajustes: ", value: client.distube.status(queue) }
                 )
