0f2df38a924acf7716abde6ceb5c605cc663e50f
Elian <eliangerardiso@gmail.com>
2022-08-04 15:00:50 -0600

    Deferred every command interaction

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -38,7 +38,7 @@ module.exports = {
                 .setTimestamp()
                 .setFooter({text: 'Memer', iconURL: client.botURL});
 
-            interaction.reply({ embeds: [embed] }).then(msg => {
+            interaction.editReply({ embeds: [embed] }).then(msg => {
                 setTimeout(() => msg.delete(), 15000)
             });
         }
